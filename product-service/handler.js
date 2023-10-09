05db7cb8f8c7cab96ff3cffe4736003efd6e5dff
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2023-10-09 21:58:41 +0400

    feat: reset main branch

--- product-service/handler.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import getProductsList from './handlers/getProductsList.js';
-import getProductsById from './handlers/getProductsById.js';
-
-export { getProductsList, getProductsById };
