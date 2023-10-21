8d64332ac4784a6adc2c0a4a53942a164f0bd8a5
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2023-10-21 16:53:20 +0400

    faet: add apigateway to collectProducts

--- product-service-ts/src/functions/collectProducts/index.ts
+++ product-service-ts/src/functions/collectProducts/index.ts
@@ -2,19 +2,19 @@ import { handlerPath } from '@libs/handler-resolver';
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
-  // events: [
-  //   {
-  //     http: {
-  //       method: 'get',
-  //       path: '/collect-products',
-  //       cors: true,
-  //       responseData: {
-  //         200: {
-  //           description: 'Successful API response',
-  //           bodyType: 'Products',
-  //         },
-  //       },
-  //     },
-  //   },
-  // ],
+  events: [
+    {
+      http: {
+        method: 'get',
+        path: '/collect-products',
+        cors: true,
+        responseData: {
+          200: {
+            description: 'Successful API response',
+            bodyType: 'Products',
+          },
+        },
+      },
+    },
+  ],
 };
