f4d4289f6be3c7f5ae967ead7beea2b074a3dac6
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2023-10-26 20:54:31 +0400

    feat: add importFileParser

--- import-service/src/functions/importProductsFile/handler.ts
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -1,14 +1,32 @@
-// import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse, errorResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
 
+const s3 = new AWS.S3({region: 'eu-central-1'})
+const BUCKET = 'aws-csv-storage';
 
-const importProductsFile = async (event) => {
+const importProductsFile = async (event: APIGatewayProxyEvent) => {
   console.log('executing importProductsFile');
+  const fileName = event.queryStringParameters.name;
+
+  if (!fileName) {
+    return errorResponse('Filename is required as a query parameter.', 400);
+  }
+
+  let signedUrl;
+
   try {
-    const items = {}
+    const params = {
+      Bucket:BUCKET,
+      Key: `uploaded/${fileName}`,
+      Expires: 60,
+      ContentType: 'text/csv'
+    }
+
+    signedUrl = s3.getSignedUrl('putObject', params)
     
-    return formatJSONResponse(items);
+    return formatJSONResponse(signedUrl);
   } catch (e) {
     console.error('Error executing importProductsFile:', e.errors || e);
     errorResponse(e);
