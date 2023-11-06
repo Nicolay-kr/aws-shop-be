7f3359b1eecdb1103ef0542fcf6fede48180cb7d
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2023-11-06 22:44:46 +0400

    fix: basicAuthorizer

--- authorization-service/src/functions/basicAuthorizer/handler.ts
+++ authorization-service/src/functions/basicAuthorizer/handler.ts
@@ -1,32 +1,38 @@
-import { middyfy } from '@libs/lambda';
-
-const basicAuthorizer = async (event, ctx, cb) => {
-  console.log('Event: ', JSON.stringify(event))
-  if(event['type'] != 'TOKEN'){
-    cb('Unauthorized')
+const basicAuthorizer = async (event) => {
+  console.log('Event: ', JSON.stringify(event));
+  
+  if (event.type !== 'TOKEN') {
+    return { 
+      statusCode: 401, 
+      body: 'Unauthorized' 
+    };
   }
 
   try {
     const authorizationToken = event.authorizationToken;
     const encodedCreds = authorizationToken.split(' ')[1];
-    const buff = Buffer.from(encodedCreds, 'base64')
+    const buff = Buffer.from(encodedCreds, 'base64');
     const plainCreds = buff.toString('utf-8').split(':');
     const username = plainCreds[0];
-    const password = plainCreds[0];
+    const password = plainCreds[1];
 
     console.log(`username: ${username}, password: ${password}`);
-    
-    const storedUsrPassword = process.env['Nicolay-kr'];
-    const effect = !storedUsrPassword || storedUsrPassword != password ? 'Deny': 'Allow';
-    const policy = generatePolicy(encodedCreds, event.methodArn, effect);
-    cb(null, policy)
 
+    const storedUsrPassword = process.env['Nicolay-kr'];
+    const effect = !storedUsrPassword || storedUsrPassword !== password ? 'Deny' : 'Allow';
+    const policy = generatePolicy(encodedCreds, event.methodArn, effect);
+
+    return policy;
   } catch (e) {
-    cb(`Unauthorized: ${e.message} `)
+    console.error(e);
+    return {
+      statusCode: 401,
+      body: `Unauthorized: ${e.message}`
+    };
   }
 };
 
-const generatePolicy = (principalId, resource, effect = 'Allow')=> {
+const generatePolicy = (principalId, resource, effect = 'Allow') => {
   return {
     principalId,
     policyDocument: {
@@ -39,10 +45,7 @@ const generatePolicy = (principalId, resource, effect = 'Allow')=> {
         }
       ]
     }
-
-  }
-
+  };
 }
 
-
-export const main = middyfy(basicAuthorizer);
+export const main = basicAuthorizer;
--- authorization-service/src/functions/basicAuthorizer/index.ts
+++ authorization-service/src/functions/basicAuthorizer/index.ts
@@ -6,7 +6,8 @@ export default {
     {
       http: {
         method: 'get',
-        path: 'hello',
+        path: 'token',
+        cors: true,
       },
     },
   ],
